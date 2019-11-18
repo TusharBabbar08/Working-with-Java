fca665ce743171b8f15ed2166c6550f5e7964a21
Tushar Babbar <53009487+TusharBabbar08@users.noreply.github.com>
2019-11-18 18:40:17 +0530

    Add files via upload

---
+++ ExcceptionHandler.java
@@ -0,0 +1,73 @@
+import java.io.*;
+class ExeptionHandler
+{
+	public static void main(String args[])throws IOException
+	 {
+	 	// BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	 	System.out.println("hello");
+	 	try
+	 	{
+	 		
+	 		Charlie();
+	 		Delta();
+	 		Aplha();
+	 		Bravo();
+
+
+	 	}
+
+
+	 	catch(ArithmeticException ae)
+	 	{
+	 		System.out.println(ae.getMessage());
+	 	}
+
+
+	 	catch(IOException ioe)
+	 	{
+	 		System.out.println(ioe);
+	 		System.out.println(ioe.getMessage());
+	 	}
+
+
+	 	catch(NumberFormatException nfe)
+	 	{
+	 		System.out.println(nfe.getMessage());
+	 	}
+
+
+
+	 	catch(StringIndexOutOfBoundsException sioe)
+	 	{
+	 		System.out.println(sioe.getMessage());
+	 	}
+		
+	}
+
+
+	static void Charlie()throws ArithmeticException
+	{
+		throw new ArithmeticException("/ by zer0");
+	}
+
+
+	static void Delta()throws IOException
+	{
+		throw new IOException(" Wrong Input");
+	}
+
+
+
+	static void Aplha()throws NumberFormatException
+	{
+		throw new NumberFormatException(" String cannot be type casted to int"); 
+	}
+
+
+
+	static void Bravo()throws StringIndexOutOfBoundsException
+	{
+		throw new StringIndexOutOfBoundsException("Wrong Length");
+	}
+
+}
